6f87f67c6351434689207bd39480c8e50394df08
Takashi Kitajima <inc@2inc.org>
2015-07-05 21:45:33 +0900

    Fixed a comments area bug.

--- src/content.php
+++ src/content.php
@@ -1,10 +1,10 @@
 <?php
 /**
- * Version    : 1.1.0
+ * Version    : 1.1.1
  * Author     : Takashi Kitajima
  * Author URI : http://2inc.org
  * Created    : April 17, 2015
- * Modified   : July 1, 2015
+ * Modified   : July 5, 2015
  * License    : GPLv2
  * License URI: http://www.gnu.org/licenses/gpl-2.0.html
  */
@@ -22,7 +22,11 @@
 	<?php do_action( 'habakiri_after_entry_content' ); ?>
 	<?php Habakiri::the_link_pages(); ?>
 	<?php Habakiri::the_related_posts(); ?>
-	<?php comments_template( '', true ); ?>
+	<?php
+	if ( comments_open() || pings_open() || get_comments_number() ) {
+		comments_template( '', true );
+	}
+	?>
 </article>
 
 <?php elseif ( is_search() ) : ?>
